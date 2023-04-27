187d3aacc3f742c8f0e27520a14149931a5280e6
lkscomk <100165295+lkscomk@users.noreply.github.com>
2023-04-27 15:22:56 +0000

    atualiza codigo

--- main.py
+++ main.py
@@ -1,69 +1,61 @@
-#codigo para verificacao de senha nivel intermediario 
-#lukas rocha Rodrigues
-
 def pedir_senha(senha, **options):
-  maiusculas = options.get('maiusculas')
-  numeros = options.get('numeros')
-  minusculas = options.get('minusculas')
-  simbolos = options.get('simbolos')
-  quantidade = options.get('quantidade')
-  confirmacao = options.get('confirmacao')
+    maiusculas = options.get('maiusculas')
+    numeros = options.get('numeros')
+    minusculas = options.get('minusculas')
+    simbolos = options.get('simbolos')
+    quantidade = options.get('quantidade')
+    confirmacao = options.get('confirmacao')
   
-  quant = True
-  mensagem = []
-  if quantidade:
-    quant = True if len(senha) >= quantidade else False
-    mensagem.append('' if len(senha) >= quantidade else 'Senha menor que '+str(quantidade)+' caracteres. ')
+    quant = True
+    mensagem = []
+    if quantidade:
+        quant = True if len(senha) >= quantidade else False
+        mensagem.append('' if len(senha) >= quantidade else f'Senha menor que {quantidade} caracteres. ')
 
-  num = True
-  if numeros:
-    num = 0
-    for x in range(10):
-      num += int(senha.count(str(x)))
-    num = True if num>0 else False
-    mensagem.append('' if num > 0 else 'Ausência de valores númericos. ')
+    num = True
+    if numeros:
+        num = 0
+        for x in range(10):
+            num += int(senha.count(str(x)))
+        num = True if num > 0 else False
+        mensagem.append('' if num > 0 else 'Ausência de valores numéricos. ')
     
-  l_letras = ['a', 'b', 'c', 'd','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','ç']
-  mas = True
-  if maiusculas:
-    mas = 0
-    for x in range(len(l_letras)):
-      mas += int(senha.count(l_letras[x].upper()))
-    mas = True if mas>0 else False
-    mensagem.append('' if mas>0 else 'Ausência de Letras Maiúsculas. ')
+    l_letras = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'ç']
+    mas = True
+    if maiusculas:
+        mas = 0
+        for x in range(len(l_letras)):
+            mas += int(senha.count(l_letras[x].upper()))
+        mas = True if mas > 0 else False
+        mensagem.append('' if mas > 0 else 'Ausência de letras maiúsculas. ')
 
-  letras = True
-  if minusculas:
-    letras = 0
-    for x in range(len(l_letras)):
-      letras += int(senha.count(l_letras[x]))
-    letras = True if letras>0 else False
-    mensagem.append('' if mas>0 else 'Ausência de Letras Minúsculas. ')
+    letras = True
+    if minusculas:
+        letras = 0
+        for x in range(len(l_letras)):
+            letras += int(senha.count(l_letras[x]))
+        letras = True if letras > 0 else False
+        mensagem.append('' if letras > 0 else 'Ausência de letras minúsculas. ')
 
-  simb = True
-  l_simbolos=['!','@','#','/','$','%','&','*','(',')']
-  if simbolos:
-    simb = 0
-    for x in range(len(l_simbolos)):
-      simb += int(senha.count(l_simbolos[x]))
-    simb = True if simb > 0 else False
-    mensagem.append('' if mas>0 else 'Ausência de Símbolos. ')
+    simb = True
+    l_simbolos = ['!', '@', '#', '/', '$', '%', '&', '*', '(', ')']
+    if simbolos:
+        simb = 0
+        for x in range(len(l_simbolos)):
+            simb += int(senha.count(l_simbolos[x]))
+        simb = True if simb > 0 else False
+        mensagem.append('' if simb > 0 else 'Ausência de símbolos. ')
 
-  conf = True
-  if confirmacao:
-    conf = True if senha == confirmacao else False
-    mensagem.append('' if senha == confirmacao else 'As senhas precisam se iguais.')
+    conf = True
+    if confirmacao:
+        conf = True if senha == confirmacao else False
+        mensagem.append('' if senha == confirmacao else 'As senhas precisam ser iguais.')
 
-  response = True if quant and num and mas and letras and simb and conf else False
-  print(quant, num, mas, letras, simb, conf)
-  #verficar_tamanho(senha)
+    response = True if quant and num and mas and letras and simb and conf else False
 
-  return {
-    'resposta': response,
-    'mensagemError': mensagem
-  }
+    return {
+        'resposta': response,
+        'mensagemError': mensagem
+    }
 
-
-
-
-print(pedir_senha('@albert123N', confirmacao='@albert123N', quantidade=8, numeros=True, maiusculas=True, minusculas=True, simbolos=True))
+print(pedir_senha('@albert123n', confirmacao='@albert123n', quantidade=8, numeros=True, maiusculas=True, minusculas=True, simbolos=True))
